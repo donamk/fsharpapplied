69eed50649e3dfa52969618eb4c8e8fb3b90dcf7
Donam Kim <sstmih@gmail.com>
2016-11-10 07:42:41 +1100

    GithubMockingApiServer End
    Repos.json has wrong format so cut off the part

--- GithubMockApiServer/Program.fs
+++ GithubMockApiServer/Program.fs
@@ -1,7 +1,19 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open System.IO
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let json fileName =
+        let content = File.ReadAllText fileName
+        content.Replace("\r", "").Replace("\n", "")
+        |> OK >=> Writers.setMimeType "application/json"
+    
+    let user = pathScan "/users/%s" (fun _ -> "User.json" |> json)
+    let repos = pathScan "/users/%s/repos" (fun _ -> "Repos.json" |> json)
+    let mockApi = choose [repos; user]
+
+    startWebServer defaultConfig mockApi
+    0
